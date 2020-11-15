2e434763fe04a8daadf24c4203804c58f8fa4023
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2020-11-15 14:26:34 +0100

    Add Main class with imports

---
+++ src/default/Main.java
@@ -0,0 +1,18 @@
+import java.util.Calendar;
+import java.util.List;
+
+import javax.jdo.Extent;
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
+import javax.jdo.Transaction;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
