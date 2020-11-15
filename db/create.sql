26211c195b7fefbe10f777c9449e337af57ab1bd
David <33113652+DavidLopezLoureiro@users.noreply.github.com>
2020-11-15 13:48:55 +0100

    Update create.sql

--- db/create.sql
+++ db/create.sql
@@ -1,11 +1,11 @@
-/* DELETE 'Easybooking' database*/
-DROP SCHEMA Easybooking;
+/* DELETE 'easybooking' database*/
+DROP SCHEMA easybooking;
 /* DELETE USER 'products_user' AT LOCAL SERVER*/
 DROP USER 'products_user'@'%';
 
-/* CREATE ''Easybooking' DATABASE */
-CREATE SCHEMA Easybooking;
+/* CREATE ''easybooking' DATABASE */
+CREATE SCHEMA easybooking;
 /* CREATE THE USER 'products_user' AT LOCAL SERVER WITH PASSWORD 'password' */
 CREATE USER 'products_user'@'%' IDENTIFIED BY 'password';
 /* GRANT FULL ACCESS TO THE DATABASE FOR THE USER 'products_user' AT LOCAL SERVER*/
-GRANT ALL ON Easybooking.* TO 'products_user'@'%';
+GRANT ALL ON easybooking.* TO 'products_user'@'%';
