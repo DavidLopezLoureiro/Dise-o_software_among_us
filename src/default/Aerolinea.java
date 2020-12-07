1e34184777fb52a2674bc63be4794a621b21224f
David <33113652+DavidLopezLoureiro@users.noreply.github.com>
2020-12-07 15:59:25 +0100

    Update Aerolinea.java

--- src/default/Aerolinea.java
+++ src/default/Aerolinea.java
@@ -1,35 +1,40 @@
+package EasyBooking;
 import javax.jdo.annotations.PersistenceCapable;
 
 @PersistenceCapable
 public class Aerolinea {
-	private String codigo_de_aerolinea;
-	private String nombre_de_aerolinea;
+	private String codigoDeAerolinea;
+	private String nombreDeAerolinea;
 	
 	
-	public Aerolinea(String codigo_de_aerolinea, String nombre_de_aerolinea) {
+	public Aerolinea(String codigoDeAerolinea, String nombreDeAerolinea) {
 		super();
-		this.codigo_de_aerolinea = codigo_de_aerolinea;
-		this.nombre_de_aerolinea = nombre_de_aerolinea;
+		this.codigoDeAerolinea = codigoDeAerolinea;
+		this.nombreDeAerolinea = nombreDeAerolinea;
+	}
+	
+	public Aerolinea() {
+		super();
 	}
 
 
-	public String getCodigo_de_aerolinea() {
-		return codigo_de_aerolinea;
+	public String getcodigoDeAerolinea() {
+		return codigoDeAerolinea;
 	}
 
 
-	public void setCodigo_de_aerolinea(String codigo_de_aerolinea) {
-		this.codigo_de_aerolinea = codigo_de_aerolinea;
+	public void setcodigoDeAerolinea(String codigoDeAerolinea) {
+		this.codigoDeAerolinea = codigoDeAerolinea;
 	}
 
 
-	public String getNombre_de_aerolinea() {
-		return nombre_de_aerolinea;
+	public String getnombreDeAerolinea() {
+		return nombreDeAerolinea;
 	}
 
 
-	public void setNombre_de_aerolinea(String nombre_de_aerolinea) {
-		this.nombre_de_aerolinea = nombre_de_aerolinea;
+	public void setnombreDeAerolinea(String nombreDeAerolinea) {
+		this.nombreDeAerolinea = nombreDeAerolinea;
 	}
 	
 	
