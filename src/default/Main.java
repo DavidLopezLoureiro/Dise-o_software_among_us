39910d81316a2b97c996bec34667f80be9ffcc66
David <33113652+DavidLopezLoureiro@users.noreply.github.com>
2020-12-07 16:00:40 +0100

    Update Main.java

--- src/default/Main.java
+++ src/default/Main.java
@@ -1,4 +1,5 @@
-import javax.jdo.Extent;
+package EasyBooking;
+
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
@@ -22,21 +23,69 @@ public class Main {
 		    
 		    String[] nombres = {"David", "Jon", "Unai", "Javier"};
 		    
-		    Aerolinea aerolinea = new Aerolinea("1","Luftansa");
-		    Aeropuerto aeropuerto_s = new Aeropuerto("Bilbao","a1");	
-		    Aeropuerto aeropuerto_l = new Aeropuerto("Madrid","a2");
+		    Aerolinea aerolinea = new Aerolinea();
 		    
-		    CreditCard targeta = new CreditCard(1, "Yo", 12022020);
-		    Pago pago = new Pago(12022020);
-		    PayPal paypal = new PayPal("usuario");
-		    Usuario usuario = new Usuario("@gmail.com", "google", aeropuerto_s, paypal);
-		    Vuelo vuelo = new Vuelo("v1", 1212, 1212, 40, 20, aerolinea, aeropuerto_s, aeropuerto_l);
-		    Reserva reserva = new Reserva(usuario, 4, nombres , 12.9f, 12032020, vuelo);
+			    aerolinea.setcodigoDeAerolinea("1");
+			    aerolinea.setnombreDeAerolinea("Luftansa");
+		    
+		    Aeropuerto aeropuerto_s = new Aeropuerto();	
+		   
+		    	aeropuerto_s.setNombre("Bilbao");
+		    	aeropuerto_s.setCodigo("a1");
+		    
+		    Aeropuerto aeropuerto_l = new Aeropuerto();
+		    
+			    aeropuerto_l.setNombre("Madrid");
+		    	aeropuerto_l.setCodigo("a2");
+		    
+		    
+		    CreditCard targeta = new CreditCard();
+		    
+		    	targeta.setnumeroDeTargeta(1);
+		    	targeta.setnombreDelTitular("Yo");
+		    	targeta.setfechaDeExpiracion(12022020);
+		    
+		    PayPal paypal = new PayPal();
+			    
+		    	paypal.setnombreDeUsuario("usuario");
+		    	
+		    Pago pago = new Pago();
+		    
+		    	pago.setFecha(12022020);
+		    	pago.setMetodoDePago(paypal);
+		    
+		    Usuario usuario = new Usuario();
+		    
+		    	usuario.setEmail("@gmail.com");
+		    	usuario.setredSocial("google");
+		    	usuario.setHabitual(aeropuerto_s);
+		    	usuario.setMetodo(paypal);
+		    
+		    Vuelo vuelo = new Vuelo();
+		    
+		    	vuelo.setcodigoDeVuelo("v1");
+		    	vuelo.setFechaYHoraDeSalida(1212);
+		    	vuelo.setfechaYHoraDeLlegada(1212);
+		    	vuelo.setasientos_totales(40);
+		    	vuelo.setasientosLibres(20);
+		    	vuelo.setAerolinea(aerolinea);
+		    	vuelo.setSalida(aeropuerto_s);
+		    	vuelo.setLlegada(aeropuerto_l);
+		    
+		    Reserva reserva = new Reserva();
+		    
+		    	reserva.setUsuario(usuario);
+		    	reserva.setnumeroDeAsientos(4);
+		    	reserva.setnombreDeLosPasajeros(nombres);
+		    	reserva.setPrecio(12.9f);
+		    	reserva.setFecha(12032020);
+		    	reserva.setVuelo(vuelo);
+		    	reserva.setPago(pago);
+		    
 		    
 		    persistentManager.makePersistent(aerolinea);
 		    persistentManager.makePersistent(aeropuerto_s);
 		    persistentManager.makePersistent(aeropuerto_l);
-		    
 		    persistentManager.makePersistent(targeta);
 		    persistentManager.makePersistent(pago);
 		    persistentManager.makePersistent(paypal);
@@ -44,18 +93,19 @@ public class Main {
 		    persistentManager.makePersistent(vuelo);
 		    persistentManager.makePersistent(reserva);
 		    
-		    System.out.println("+ Inserted aerolinea into db: " + aerolinea.getNombre_de_aerolinea());
+		    System.out.println("+ Inserted aerolinea into db: " + aerolinea.getnombreDeAerolinea());
 		    System.out.println("+ Inserted aeropuerto into db: " + aeropuerto_s.getNombre());
 		    System.out.println("+ Inserted aeropuerto into db: " + aeropuerto_l.getNombre());
 		    
-		    System.out.println("+ Inserted targeta into db: " + targeta.getNombre_del_titular());
+		    System.out.println("+ Inserted targeta into db: " + targeta.getnombreDelTitular());
 		    System.out.println("+ Inserted pago into db: " + pago.getFecha());
-		    System.out.println("+ Inserted paypal into db: " + paypal.getNombre_de_usuario());
+		    System.out.println("+ Inserted paypal into db: " + paypal.getnombreDeUsuario());
 		    System.out.println("+ Inserted usuario into db: " + usuario.getEmail());
-		    System.out.println("+ Inserted vuelo into db: " + vuelo.getCodigo_de_vuelo());
-		    System.out.println("+ Inserted reserva into db: " + reserva.getNumero_de_asientos());
+		    System.out.println("+ Inserted vuelo into db: " + vuelo.getcodigoDeVuelo());
+		    System.out.println("+ Inserted reserva into db: " + reserva.getnumeroDeAsientos());
 		    
 		    transaction.commit();
+		    
 		} catch(Exception ex) {
 			System.err.println("* Exception inserting data into db: " + ex.getMessage());
 		} finally {		    
@@ -73,11 +123,12 @@ public class Main {
 		try {
 			transaction.begin();
 
-		    @SuppressWarnings("unchecked")
-			Query<Usuario> productsQuery = persistentManager.newQuery("SELECT FROM " + Usuario.class.getName() + " WHERE email == '@gmail.com'");
+			Query<CreditCard> productsQuery = persistentManager.newQuery(CreditCard.class);
+		    productsQuery.setFilter("numeroDeTargeta == 1");
 		    
-		    for (Usuario usuario : productsQuery.executeList()) {
-		        System.out.println("? Selected product from db: " + usuario.getEmail());
+		    
+		    for (CreditCard targeta : productsQuery.executeList()) {
+		        System.out.println("? Selected product from db: " + targeta.getnumeroDeTargeta());
 		    }
 		    	
 		    transaction.commit();
@@ -98,22 +149,20 @@ public class Main {
 		try {
 			transaction.begin();
 			
-		    @SuppressWarnings("unchecked")
-		    Query<Usuario> productsQuery = persistentManager.newQuery("SELECT FROM " + Usuario.class.getName() + " WHERE email == '@gmail.com'");
+		   
+			Query<CreditCard> productsQuery = persistentManager.newQuery(CreditCard.class);
+		    productsQuery.setFilter("numeroDeTargeta == 1");
+		    	
 		    
-		    for (Usuario usuario : productsQuery.executeList()) {
-		        System.out.println("? Selected product from db: " + usuario.getEmail());
+		    for (CreditCard targeta : productsQuery.executeList()) {
+		        System.out.println("? Selected product from db: " + targeta.getnumeroDeTargeta());
+		        System.out.println("- Deleted inventory from db: " + targeta.getnumeroDeTargeta());
+		    	persistentManager.deletePersistent(targeta);
 		    }
-			
-			Extent<Usuario> inventoryExtent = persistentManager.getExtent(Usuario.class);
-		    
-		    for (Usuario usuario : inventoryExtent) {
-		    	System.out.println("- Deleted inventory from db: " + usuario.getEmail());
-		    	persistentManager.deletePersistent(usuario);			        
-		    }
+		 
 		    transaction.commit();
-		} catch(Exception ed) {
-			System.err.println("* Exception deleting data from DB: " + ed.getMessage());
+		} catch(Exception ex) {
+			System.err.println("* Exception deleting data from DB: " + ex.getMessage());
 		} finally {
 			if (transaction.isActive()) {
 		        transaction.rollback();
@@ -127,3 +176,6 @@ public class Main {
 	}
 }
 }
+
+
+
